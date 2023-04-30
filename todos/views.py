fc45f637e0c798a3a0e952857278e6b77e5f438b
Saurav Kumar <66282953+sauravsaran99@users.noreply.github.com>
2023-05-01 01:05:06 +0530

    all code

---
+++ todos/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializers
+
+class ListTodo(generics.ListAPIView): 
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers
